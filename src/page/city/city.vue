572962989ade79c558ca78a1472a30593235a8a1
xiezx <xiezx@szkingdom.com>
2021-03-28 19:19:56 +0800

    test

--- src/page/city/city.vue
+++ src/page/city/city.vue
@@ -8,12 +8,12 @@
                 <input type="search" name="city" placeholder="输入学校、商务楼、地址" class="city_input input_style" required v-model="inputValue">
             </div>
             <div>
-                <input type="submit" name="submit" class="city_submit input_style" value="提交">
+                <input type="submit" @click="getPois" name="submit" class="city_submit input_style" value="提交">
             </div>
         </form>
         <div class="pois_search_history">搜索历史</div>
         <ul class="getpois_ul">
-            <li v-for="(item, index) in getPoisList" :key="index">
+            <li v-for="(item, index) in poisList" :key="index">
                 <h4 class="pois_name">{{item.name}}</h4>
                 <p class="pois_address">{{item.address}}</p>
             </li>
@@ -33,7 +33,7 @@ export default {
             cityId: '',
             cityName: '',
             inputValue: '',
-            getPoisList: [],
+            poisList: [],
         }
     },
     mounted() {
@@ -41,6 +41,15 @@ export default {
         currentcity(this.cityId).then(res => {
             this.cityName = res.name;
         })
+    },
+    methods: {
+        getPois() {
+            if (this.inputValue) {
+                searchplace(this.cityId, this.inputValue).then(res => {
+                    this.poisList = res;
+                })
+            }
+        }
     }
 }
 </script>
@@ -76,6 +85,28 @@ export default {
         .city_submit {
             background-color: $blue;
             color: #fff;
+        }
+    }
+}
+.pois_search_history {
+    border-top: 1px solid $bc;
+    padding-left: 0.5rem;
+    @include font(0.475rem, 0.8rem);
+}
+.getpois_ul {
+    border-top: 1px solid $bc;
+    li {
+        padding-top: 0.5rem;
+        border-bottom: 1px solid $bc;
+        .pois_name {
+            width: 90%;
+            margin: 0 auto 0.35rem;
+            @include sc(0.65rem, #333);
+        }
+        .pois_address {
+            width: 90%;
+            margin: 0 auto 0.55rem;
+            @include sc(0.45rem, #666);
         }
     }
 }
